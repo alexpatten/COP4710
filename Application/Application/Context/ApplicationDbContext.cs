97fc31ceacb51ef44a74a1c3ad3f76faa783458d
Alex Patten <103220430+alexpatten@users.noreply.github.com>
2023-10-05 10:51:15 -0400

    Packages

--- Application/Application/Context/ApplicationDbContext.cs
+++ Application/Application/Context/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Application.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Context
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<Patient> Patients { get; set; }
     }
 }
