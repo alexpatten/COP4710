69054df1ad8f263e0e484e6060eb05b61814dda7
Alex Patten <103220430+alexpatten@users.noreply.github.com>
2023-10-12 13:02:41 -0400

    Major Updates
    -Recreated VS project
    -Fixed major bugs for POST/GET to and from DB
    -Added DB backup

---
+++ Clinic/Clinic/Controllers/RegistrationController.cs
@@ -0,0 +1,41 @@
+﻿using Clinic.Models;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clinic.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Registration(RegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.SelectedUserType == "Doctor")
+                {
+                    // Process Doctor registration
+                    ClinicEntities db = new ClinicEntities();
+                    db.Doctors.Add(model.Doctor);
+                    db.SaveChanges();
+                }
+                else if (model.SelectedUserType == "Patient")
+                {
+                    // Process Patient registration
+                    ClinicEntities db = new ClinicEntities();
+                    db.Patients.Add(model.Patient);
+                    db.SaveChanges();
+                }
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ Clinic/Clinic/Models/RegistrationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clinic.Models
+{
+    public partial class RegistrationModel
+    {
+        public Patient Patient { get; set; }
+        public Doctor Doctor { get; set; }
+
+        // Add a property to indicate the selected user type
+        public string SelectedUserType { get; set; }
+    }
+}
